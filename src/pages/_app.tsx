e568ce91e1d4ad16377c3c7df70421a9cedd257a
salaudeen01 <salaudeenhameed4real@gmail.com>
2025-10-27 11:18:29 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import { Toaster } from "@/components/ui/toaster";
 import QueryProvider from "@/providers/QueryProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryProvider> <Component {...pageProps} /></QueryProvider>
+    <QueryProvider> 
+      <Toaster />
+      <Component {...pageProps} />
+    </QueryProvider>
  
 );
 }
