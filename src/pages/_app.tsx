8b18a9495fd9ff571a182d1edbc6e07739d22716
salaudeen01 <salaudeenhameed4real@gmail.com>
2025-10-27 11:33:17 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,8 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryProvider> 
-      <Toaster />
       <Component {...pageProps} />
+      <Toaster />
     </QueryProvider>
  
 );
