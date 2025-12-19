8bd81a358188cb6cdcb4d56f6707ecd80cf2d178
salaudeen01 <salaudeenhameed4real@gmail.com>
2025-12-19 17:00:48 +0100

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Article } from "../../type";
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fetchArticle = async (slug: string): Promise<Article> => {
+  const baseUrl =
+    typeof window === "undefined"
+      ? process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3001"
+      : "";
+
+  const res = await axios.get(`${baseUrl}/api/news/${slug}`);
+  return res.data.data;
+};
